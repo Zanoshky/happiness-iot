121049d8f3f3bf7189a87979c1ac11a67348f549
Marko Zanoski <marko.zanoski@mysugr.com>
2019-11-12 15:25:15 +0100

    feat(baseline): Initial commit

---
+++ src/src.ino
@@ -0,0 +1,32 @@
+/*
+  Codename Happiness
+  Language: Arduino
+
+  This program reads various sensors
+  -> Humidity
+  -> Temperature
+  -> Dust
+  -> Volume
+  -> Light
+  
+  And sends an HTTP request every seconds with current values.
+
+  The circuit:
+  -> TODO
+
+  Created: 2019-11-12
+  by Marko Zanoski <zanoski.marko@gmail.com>
+  assisted by 
+  
+  This example code is in the public domain on Github.
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
