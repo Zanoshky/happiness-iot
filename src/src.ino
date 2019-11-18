1b377b505a6fa3377c09428eb96fa89ca318322a
Marko Zanoski <marko.zanoski@mysugr.com>
2019-11-18 19:16:21 +0100

    Reading temperature and humidity level

--- src/src.ino
+++ src/src.ino
@@ -4,8 +4,8 @@
  * 
  * This program reads various sensors
  * <ul>
- * <li>Humidity</li>
- * <li>Temperature</li>
+ * <li>Humidity - DHT22</li>
+ * <li>Temperature - DHT22</li>
  * <li>Dust</li>
  * <li>Volume - SoundSensor LM386</li>
  * <li>Light - BH1750</li>
@@ -28,21 +28,20 @@
  */
 #include <Wire.h>
 #include <BH1750.h>
+#include <DHT.h>
 
 /*
  * List of constants
  */
-#define PIN_D_HUMIDITY 1
-#define PIN_D_TEMPERATURE 2
+#define PIN_D_TEMP_N_HUM 2
 #define PIN_D_DUST 3
 #define PIN_A_VOLUME 0
 #define PIN_A_LIGHT 5
-#define TEAM_NAME "Heisenberg"
-/*
- * Code initialization
- * TODO
- */
 
+#define TEAM_NAME "Heisenberg"
+
+#define DHTPIN 7
+#define DHTTYPE DHT22
 
 /*
  * Code variables
@@ -52,6 +51,7 @@ float temperatureValue;
 float dustValue;
 long soundMeter;
 BH1750 lightMeter;
+DHT dht(PIN_D_TEMP_N_HUM, DHTTYPE);
 
 /*
  * Initial code block that executes on startup / booting
@@ -62,6 +62,7 @@ void setup()
   Serial.begin(9600);
 
   lightMeter.begin();
+  dht.begin();
 
   Serial.print("Starting Codename Happiness for ");
   Serial.print(TEAM_NAME);
@@ -73,13 +74,17 @@ void setup()
  * Sketch uses 3620 bytes (11%) of program storage space. Maximum is 32256 bytes.
  * Global variables use 252 bytes (12%) of dynamic memory, leaving 1796 bytes for local variables. Maximum is 2048 bytes.
  * 
- * With Sound Reading
+ * With Sound Reading - LM386
  * Sketch uses 3810 bytes (11%) of program storage space. Maximum is 32256 bytes.
  * Global variables use 256 bytes (12%) of dynamic memory, leaving 1792 bytes for local variables. Maximum is 2048 bytes.
  * 
- * With Light Reading
+ * With Light Reading - BH1750
  * Sketch uses 5890 bytes (18%) of program storage space. Maximum is 32256 bytes.
  * Global variables use 514 bytes (25%) of dynamic memory, leaving 1534 bytes for local variables. Maximum is 2048 bytes.
+ * 
+ * With Temperature and Humidity - DHT22
+ * Sketch uses 7424 bytes (23%) of program storage space. Maximum is 32256 bytes.
+ * Global variables use 541 bytes (26%) of dynamic memory, leaving 1507 bytes for local variables. Maximum is 2048 bytes.
  */
 
 /*
@@ -89,7 +94,7 @@ void loop()
 {  
     delay(1000);
 
-    // Sample sound / volume
+    // Sample sound / volume --------------------------------
     soundMeter = 0;
     for(int i=0; i<32; i++)
     {
@@ -98,9 +103,20 @@ void loop()
 
     soundMeter >>= 5;
 
+    // ------------------------------------------------------
+
     // Sample light
     uint16_t lightMeterReading = lightMeter.readLightLevel();
+
+    // ------------------------------------------------------
     
+    // Sample Temperature and Humidity ----------------------
+    humidityValue = dht.readHumidity();
+    temperatureValue = dht.readTemperature();
+
+    // ------------------------------------------------------
+    
+    // Debug print
     Serial.print("Humidity: ");
     Serial.print(humidityValue);
     Serial.println("");
