f69adc8e04de622b0e2692e7918a688577e2a671
Andreea <bluu.andreea@yahoo.com>
2020-01-16 16:58:48 +0100

    Wifi

--- src/src.ino
+++ src/src.ino
@@ -37,6 +37,7 @@
 #include <BH1750.h>
 #include <Adafruit_BME280.h>
 
+
 /*
  * List of constants for digital pins
  */
@@ -77,28 +78,40 @@ float ratio = 0;
 float concentration = 0;
 int smokeAnalogSensor = A0;
 int sensorThres = 400;
+const byte rxPin = 4; // Wire this to Tx Pin of ESP8266
+const byte txPin = 5; // Wire this to Rx Pin of ESP8266
 
+String AP = "<SSID>";       // AP NAME
+String PASS = "<PASSWORD>";     // AP PASSWORD
+String API = "RHOOBC0BQOFJ7GE0";   // Write API KEY
+String HOST = "api.thingspeak.com";
+String PORT = "80";
+int countTrueCommand;
+int countTimeCommand; 
+boolean found = false; 
+int valSensor = 1;
+
+
+//ESP8266
+SoftwareSerial ESP8266 (rxPin, txPin);
 //temperature & humidity
+
+
 DHT dht(PIN_D_TEMP_N_HUM, DHTTYPE);
 
 //light
 BH1750 lightMeter;
 
-//Wifi
-//SoftwareSerial esp8266(0, 1);
 
 //digital temperature & humidity
 Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO,  BME_SCK);
 
 
-/*
- * Initial code block that executes on startup / booting
- */
 void setup()
 {
   // Define USB
   Serial.begin(9600);
-  //esp8266.begin(115200); 
+  ESP8266.begin(9600); 
 
   dht.begin();
   lightMeter.begin();
@@ -114,99 +127,122 @@ void setup()
   if (!bme.begin()) {  
     Serial.println("Could not find a valid BME280 sensor, check wiring!");
   }
+
+  sendCommand("AT",5,"OK");
+  sendCommand("AT+CWMODE=1",5,"OK");
+  sendCommand("AT+CWJAP=\""+ AP +"\",\""+ PASS +"\"",20,"OK");
+
 }
 
-/**
- * Compilaiton result
- * Sketch uses 3620 bytes (11%) of program storage space. Maximum is 32256 bytes.
- * Global variables use 252 bytes (12%) of dynamic memory, leaving 1796 bytes for local variables. Maximum is 2048 bytes.
- * 
- * With Sound Reading - LM386
- * Sketch uses 3810 bytes (11%) of program storage space. Maximum is 32256 bytes.
- * Global variables use 256 bytes (12%) of dynamic memory, leaving 1792 bytes for local variables. Maximum is 2048 bytes.
- * 
- * With Light Reading - BH1750
- * Sketch uses 5890 bytes (18%) of program storage space. Maximum is 32256 bytes.
- * Global variables use 514 bytes (25%) of dynamic memory, leaving 1534 bytes for local variables. Maximum is 2048 bytes.
- * 
- * With Temperature and Humidity - DHT22
- * Sketch uses 7424 bytes (23%) of program storage space. Maximum is 32256 bytes.
- * Global variables use 541 bytes (26%) of dynamic memory, leaving 1507 bytes for local variables. Maximum is 2048 bytes.
- * 
- * Code optimization from int to byte
- * Sketch uses 7416 bytes (22%) of program storage space. Maximum is 32256 bytes.
- * Global variables use 537 bytes (26%) of dynamic memory, leaving 1511 bytes for local variables. Maximum is 2048 bytes.
- */
+void sendCommand(String command, int maxTime, char readReplay[]) {
+  Serial.print(countTrueCommand);
+  Serial.print(". at command => ");
+  Serial.print(command);
+  Serial.print(" ");
+  while(countTimeCommand < (maxTime*1))
+  {
+    ESP8266.println(command);//at+cipsend
+    if(ESP8266.find(readReplay))//ok
+    {
+      found = true;
+      break;
+    }
+  
+    countTimeCommand++;
+  }
+  
+  if(found == true)
+  {
+    Serial.println("Works");
+    countTrueCommand++;
+    countTimeCommand = 0;
+  }
+  
+  if(found == false)
+  {
+    Serial.println("Fail");
+    countTrueCommand = 0;
+    countTimeCommand = 0;
+  }
+  
+  found = false;
+ }
 
-/*
- * Main code logic that loop indefinitely
- */
-void loop()
-{  
-    delay(1000);
 
-    //light BH1750-------------------------------------------
-    uint16_t lux = lightMeter.readLightLevel();
-    Serial.print("Light: ");
-    Serial.print(lux);
-    Serial.println(" lx");
+String readHumidityDH11(){
+    humidityValue = dht.readHumidity();
+    return String(humidityValue);
+}
 
-    // Sample sound -----------------------------------------
-    soundMeter = 0;
+String readTemperatureDH11(){
+    temperatureValue = dht.readTemperature();
+    return String(temperatureValue);
+}
+
+
+
+String readDigitalTemperature() {
+  digitalTemp = bme.readTemperature();
+  return String(digitalTemp);
+}
+
+String readDigitalHumidity() {
+  digitalHumidity = bme.readHumidity();
+  return String(digitalHumidity);
+}
+
+String readDigitalPressure() {
+    digitalPressure = bme.readPressure();
+  return String(digitalPressure);
+}
+
+
+
+String gasDetection() {
+  int analogSensor = analogRead(smokeAnalogSensor);
+  return String(analogSensor);
+}
+
+
+
+String soundLevelDetection() {
+  soundMeter = 0;
     for(byte i=0; i<32; i++)
     {
         soundMeter += analogRead(PIN_A_VOLUME);
     }
     soundMeter >>= 5;
     soundMeter = 20 * log10(analogRead(soundMeter));
-    Serial.print("Sound: ");
-    Serial.print(soundMeter);
-    Serial.println("");
+    return String(soundMeter);
+}
 
-    //DHT-----------------------------------------------------
-    humidityValue = dht.readHumidity();
-    temperatureValue = dht.readTemperature();
-    Serial.print("Humidity: ");
-    Serial.print(humidityValue);
-    Serial.println("");
-    Serial.print("Temperature: ");
-    Serial.print(temperatureValue);
-    Serial.println("");
-   
 
-    //digitalBME280-------------------------------------------
-    Serial.print("Digital Values:");
-    digitalTemp = bme.readTemperature();
-    digitalPressure = bme.readPressure();
-    digitalHumidity = bme.readHumidity();
-    Serial.print("Digital Temp: ");
-    Serial.print(digitalTemp);
-    Serial.println("");
-    Serial.print("Digital Humidity: ");
-    Serial.print(digitalHumidity);
-    Serial.println("");
-    Serial.print("Digital Pressure ");
-    Serial.print(digitalPressure);
-    Serial.println("");
+String lightIntensity(){
+  uint16_t lux = lightMeter.readLightLevel();
+  return String(lux);
+}
 
-    //Smoke detection MQ2-----------------------------------
-    int analogSensor = analogRead(smokeAnalogSensor);
-    Serial.print("MQ2: ");
-    Serial.println(analogSensor);
 
-    //Dust detection-----------------------------------------
+String dustLevelDetection(){
+        //Dust detection-----------------------------------------
     duration = pulseIn(pinDust, LOW);
     lowpulseoccupancy = lowpulseoccupancy+duration;
-    if ((millis()-starttime) >= sampletime_ms) //if the sampel time = = 30s 
-    {
+    if ((millis()-starttime) >= sampletime_ms) {
       ratio = lowpulseoccupancy/(sampletime_ms*10.0);  
-      concentration = 1.1*pow(ratio,3)-3.8*pow(ratio,2)+520*ratio+0.62; 
-      Serial.print("DustConcentration = ");
-      Serial.print(concentration);
-      Serial.println(" pcs/0.01cf");
-      Serial.println("\n");
+      concentration = 1.1*pow(ratio,3)-3.8*pow(ratio,2)+520*ratio+0.62;
+      return String(concentration);
       lowpulseoccupancy = 0;
       starttime = millis();
-  }
-    
+    }
+}
+
+void loop()
+{  
+   String getData = "GET /update?api_key="+ API +"&field1="+readTemperatureDH11()+"&field2="+readHumidityDH11()+"&field3="+readDigitalTemperature()+"&field4="+readDigitalHumidity()+"&field5="+readDigitalPressure()+"&field6="+lightIntensity()+"&field7="+soundLevelDetection()+"&field8="+dustLevelDetection();
+   sendCommand("AT+CIPMUX=1",5,"OK");
+   sendCommand("AT+CIPSTART=0,\"TCP\",\""+ HOST +"\","+ PORT,15,"OK");
+   sendCommand("AT+CIPSEND=0," +String(getData.length()+4),4,">");
+   ESP8266.println(getData);delay(1500);countTrueCommand++;
+   sendCommand("AT+CIPCLOSE=0",5,"OK");
+  
 }
